05e5a1984b36a5bdd6cf889060573cdb97dbc728
J Palmer <j-mpalmer@live.com>
2023-03-23 17:37:48 -0400

    v0.5.0 more clean up, added localization support and (bad) spanish translation, condensed actions into helper class

--- auto-fastforward.js
+++ auto-fastforward.js
@@ -1,28 +1,13 @@
 import { AutoFastForward } from "./module/auto-fastforward.js";
-import { patchHelper } from "./module/patch.js";
 
 Hooks.on("init", function () {
 
-    const debouncedReload = foundry.utils.debounce( () => window.location.reload(), 100 );    
-    game.settings.register("auto-fastforward", "autoFastForward", {
-        name: "Auto Fast Forward Rolls?",
-        hint: " Requires Reload. Enable to reverse default roll behaviour.When enabled: All rolls are fast forwarded and shift with bring up the roll prompt.",
-        scope: "world",
-        config: true,
-        type: Boolean,
-        default: false,
-        onChange: debouncedReload,
-    
-    });
+    AutoFastForward.initialize();
 
 });
 
 Hooks.on("ready", function () {
 
-    const autoFastForward = game.settings.get("auto-fastforward", "autoFastForward");
-    if (autoFastForward) {
-        AutoFastForward.log("Patching Helper.isUsingFastForwardKey");
-        libWrapper.register("auto-fastforward", "game.helper.isUsingFastForwardKey", patchHelper, 'WRAPPER');
-    }
+    AutoFastForward.patch();
 
 });
--- module/auto-fastforward.js
+++ module/auto-fastforward.js
@@ -1,3 +1,5 @@
+import { patchHelper } from "./patch.js";
+
 /**
  * A class for holding utilities, mostly as a hedge for possible expansion 
  */
@@ -7,14 +9,62 @@ export class AutoFastForward {
 
     static NAME = "Auto FastForward";
 
+    static PATCHES = {
+        FASTFORWARD: {
+            target: "game.helper.isUsingFastForwardKey",
+            type: 'WRAPPER'
+        }
+    }
 
-      /**
-   * A small helper function for logging with formatting
-   * 
-   * @param  {...any} args - what to log
-   */
+    static SETTINGS = {
+        AUTO_FASTFORWARD: {
+            id: "autoFastForward",
+            title: "AUTO-FAST.settings.auto-fastforward.title",
+            hint:"AUTO-FAST.settings.auto-fastforward.hint", 
+            scope: "world",
+            type: Boolean,
+            default: false,
+            config: true
+        }
+    }
+
+    static initialize() {
+
+        const debouncedReload = foundry.utils.debounce(() => window.location.reload(), 100);
+        game.settings.register(this.ID, this.SETTINGS.AUTO_FASTFORWARD.id, {
+            name: this.SETTINGS.AUTO_FASTFORWARD.title,
+            hint: this.SETTINGS.AUTO_FASTFORWARD.hint,
+            scope: this.SETTINGS.AUTO_FASTFORWARD.scope,
+            type: this.SETTINGS.AUTO_FASTFORWARD.type,
+            default: this.SETTINGS.AUTO_FASTFORWARD.default,
+            config: this.SETTINGS.AUTO_FASTFORWARD.config,
+            onChange: debouncedReload,
+        });
+    }
+
+    static patch() {
+
+        const autoFastForward = game.settings.get(this.ID, this.SETTINGS.AUTO_FASTFORWARD.id);
+        if (autoFastForward) {
+            this.log(`${this.localize("AUTO-FAST.gen.patch")} ${this.PATCHES.FASTFORWARD.target}`);
+            libWrapper.register(this.ID, this.PATCHES.FASTFORWARD.target, patchHelper, this.PATCHES.FASTFORWARD.type);
+        }
+    }
+
+
+
+    /**
+    * A small helper function for logging with formatting
+    * 
+    * @param  {...any} args - what to log
+    */
     static log(...args) {
         console.log(`${this.NAME}  |  ${args}`);
     }
+
+    static localize(key) {
+        return game.i18n.localize(key);
+    }
+
 
 }
